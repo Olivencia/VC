c083f522d46933603bb3914e68e56f6d824c1cd6
= <=>
2017-01-28 11:21:43 +0100

    Script bash de obtención de ejecutables

---
+++ execute.bash
@@ -0,0 +1,3 @@
+g++ -ggdb T1/main.cpp -o ejercicio1 `pkg-config --cflags --libs opencv`;
+g++ -ggdb T2/main.cpp -o ejercicio2 `pkg-config --cflags --libs opencv`;
+g++ -ggdb T3/main.cpp -o ejercicio3 `pkg-config --cflags --libs opencv`;
